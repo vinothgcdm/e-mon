1518448e95489ab443dac589984fa3feab1acdf1
vinothg.cdm <vinothg.cdm@gmail.com>
2017-04-25 12:19:23 +0530

    Code cleanup  Changes to be committed: 	modified:   hw-timer-lpc1343.h 	modified:   main.c

--- hw-timer-lpc1343.h
+++ hw-timer-lpc1343.h
@@ -7,6 +7,8 @@
 	irq_setcb(IRQ_CT16B0, fun_cb);
 
 void hw_timer_init(void);
+/* We can set upto 65 milli second. If you want more time change
+ * TMR16B0PR */
 void hw_timer_setup(uint32_t milli_sec);
 void hw_timer_clean_intr(void);
 
--- main.c
+++ main.c
@@ -1,41 +1,43 @@
 #include <board.h>
+#include <stdio.h>
 #include <timer.h>
 #include <serial.h>
-#include <adc.h>
 #include <lcd.h>
-#include <stdio.h>
+#include <adc.h>
 #include "hw-timer-lpc1343.h"
 
 #define CH 1
-#define RESISTANCE 1  // Ohm
 
-static uint32_t curr_val = 0;
-static uint64_t sum = 0;
+struct adc_val {
+	uint64_t sum;
+        uint32_t curr_val;
+	uint32_t count;
+} data;
 
-void sum_current(void)
+
+void sum_current(void *timer)
 {
-        curr_val = adc_read_raw(CH);
-	sum += curr_val;
+        data.curr_val = adc_read_raw(CH);
+	data.sum += data.curr_val;
+	data.count++;
 	hw_timer_clean_intr();
 }
 
-void test(void *timer)
+void print_data(void *timer)
 {
 	static uint32_t sec = 0;
-	uint64_t l_sum = sum;
-	uint32_t l_curr_val = curr_val;
-	uint32_t milli_amp_ms;
-	uint32_t milli_amp_h;
-	char buf[10];
+	struct adc_val tmp = data;
+	uint64_t milli_amp_ms;
+	uint32_t milli_amp_h_L;
+	uint32_t milli_amp_h_R;
 
 	sec++;
 	lcd_clear();
-	printf("%04u mV\r\n", ((l_curr_val * 3300UL) >> 10) / RESISTANCE);
-	milli_amp_ms = ((l_sum * 3300UL) >> 10) / RESISTANCE;
-	milli_amp_h = milli_amp_ms / (1000 * 60 * 60);
-	sprintf(buf, "%d", milli_amp_ms % (1000 * 60 * 60));
-	/* printf("%04u mAms\r\n", milli_amp_ms); */
-	printf("%02u: %04u.%c%c mAh", sec, milli_amp_h, buf[0], buf[1]);
+	printf("%u mA\r\n", (tmp.curr_val * 3300UL) >> 10);
+	milli_amp_ms = (tmp.sum * 10 * 3300UL) >> 10;
+	milli_amp_h_L = milli_amp_ms / (1000 * 3600);
+	milli_amp_h_R = milli_amp_ms % (1000 * 3600);
+	printf("%u: %u.%u mAh", sec, milli_amp_h_L, (milli_amp_h_R * 1000) / (1000 * 3600));
 
 	timer_setup(timer, 1000);
 }
@@ -45,21 +47,20 @@ int main(void)
 	timer_t timer;
 	
 	board_init();
-	/* board_stdout(SERIAL_STDOUT); */
-	/* serial_init(); */
 	board_stdout(LCD_STDOUT);
 	lcd_init();
 	timer_init();
 	adc_init();
 
 	adc_enable(CH);
-	
-	hw_timer_init();
-	hw_timer_setcb(sum_current);
-	hw_timer_setup(1);
 
 	timer_setup(&timer, 1000);
-	timer_setcb(&timer, test);
+	timer_setcb(&timer, print_data);
+
+	hw_timer_init();
+	hw_timer_setcb(sum_current);
+	hw_timer_setup(10);
+
 	event_poll();
 	
 	return 0;
